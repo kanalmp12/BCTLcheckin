b76a907c15b07e0ad0b510d661b6c2dd4e81c2d4
kanalmp12 <kana.lmp12@gmail.com>
2026-02-18 21:43:58 +0700

    Just grab a coffee

---
+++ config.js
@@ -0,0 +1,3 @@
+// Google Apps Script Web App URL
+// Replace with your deployment URL
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyqjfz17VYFcoOvZx2KHziEk-z0okFWaLqryyUE5mnVsqXNTMHD2P5XT2N4Y_nJ-hTB1g/exec";
